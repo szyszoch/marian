b2f5b004b675075a81fb26e8c9610aa60d6b2fc8
szyszoch <pyrun97531@gmail.com>
2023-08-31 20:47:39 +0200

    test rendering two textures

--- src/main.c
+++ src/main.c
@@ -1,31 +1,8 @@
 #include <glad/glad.h>
 #include <glfw/glfw3.h>
 #include <stdio.h>
-#include <shader.h>
 #include <texture.h>
-
-const char* vertex_shader_source = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec3 aColor;\n"
-    "layout (location = 2) in vec2 aTexCoord;\n"
-    "out vec3 ourColor;\n"
-    "out vec2 TexCoord;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos, 1.0);\n"
-    "   ourColor = aColor;\n"
-    "   TexCoord = aTexCoord;\n"
-    "}\n";
-
-const char* fragment_shader_source = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "in vec3 ourColor;\n"
-    "in vec2 TexCoord;\n"
-    "uniform sampler2D ourTexture;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = texture(ourTexture, TexCoord);\n"
-    "}\n";
+#include <renderer.h>
 
 void input(GLFWwindow* window) 
 {
@@ -58,55 +35,21 @@ int main()
 
     glViewport(0, 0, 512, 480);
 
-    float vertices[] = {
-     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
-     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
-    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
-    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f
-    };
+    renderer_init();
 
-    unsigned int indices[] = {
-        0, 1, 3,
-        1, 2, 3
-    };
+    sprite_t sprite = sprite_get(SPRITE_GROUND, PALETTE_OVERWORLD_GROUND_AND_STONE);
+    texture_t texture1 = texture_load_image(&sprite);
+    sprite_destroy(sprite);
 
-    GLuint VBO;
-    glGenBuffers(1, &VBO);
-    GLuint VAO; 
-    glGenVertexArrays(1, &VAO);
-    GLuint EBO;
-    glGenBuffers(1, &EBO);
+    sprite = sprite_get(SPRITE_GROUND, PALETTE_UNDERGROUND_GROUND_AND_STONE);
+    texture_t texture2 = texture_load_image(&sprite);
+    sprite_destroy(sprite);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    vec2 pos1 = {128.0f,128.0f};
+    vec2 size1 = {128.0f, 128.0f};
 
-    shader_t shader = shader_create(vertex_shader_source, fragment_shader_source);
-    shader_bind(shader);
-
-    glBindVertexArray(VAO); 
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
-
-    palette_t palette = palette_get(PALETTE_CASTLE_GROUND_AND_STONE);
-    sprite_t sprite = sprite_get(SPRITE_GROUND, palette);
-    texture_t texture = texture_load_image(&sprite);
-    glActiveTexture(texture.id);
-
-    glBindTexture(GL_TEXTURE_2D, texture.id);
-    glBindVertexArray(VAO);
+    vec2 pos2 = {256.0f,128.0f};
+    vec2 size2 = {128.0f, 128.0f};
 
     while(!glfwWindowShouldClose(window)) {
         input(window);
@@ -114,11 +57,10 @@ int main()
         glfwPollEvents();
         glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        renderer_render(texture1, pos1, size1);
+        renderer_render(texture2, pos2, size2);
     }
 
-    sprite_destroy(sprite);
-    shader_destroy(shader);
     glfwTerminate();
     return 0;
 }
