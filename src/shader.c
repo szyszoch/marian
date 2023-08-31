637bcbdb1f8b0e4c4d252e69a924fe56def6f6d0
szyszoch <pyrun97531@gmail.com>
2023-08-31 20:42:22 +0200

    update shader functions

--- src/shader.c
+++ src/shader.c
@@ -2,16 +2,16 @@
 
 static GLuint compile(GLenum type, const char* source)
 {
-    GLuint shader = glCreateShader(type);
-    glShaderSource(shader, 1, &source, NULL);
-    glCompileShader(shader);
+    GLuint shader = GL(glCreateShader(type));
+    GL(glShaderSource(shader, 1, &source, NULL));
+    GL(glCompileShader(shader));
 
     GLint success;
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    GL(glGetShaderiv(shader, GL_COMPILE_STATUS, &success));
 
     if (!success) {
         char log[512];
-        glGetShaderInfoLog(shader, 512, NULL, log);
+        GL(glGetShaderInfoLog(shader, 512, NULL, log));
         fprintf(stderr, "%s\n", log);
     }
 
@@ -24,63 +24,38 @@ shader_t shader_create(const char* vertex_shader_source, const char* fragment_sh
     GLuint fragment_shader = compile(GL_FRAGMENT_SHADER, fragment_shader_source);
     
     shader_t new_shader;
-    new_shader.id = glCreateProgram();
+    new_shader.id = GL(glCreateProgram());
 
-    glAttachShader(new_shader.id, vertex_shader);
-    glAttachShader(new_shader.id, fragment_shader);
-    glLinkProgram(new_shader.id);
+    GL(glAttachShader(new_shader.id, vertex_shader));
+    GL(glAttachShader(new_shader.id, fragment_shader));
+    GL(glLinkProgram(new_shader.id));
     
-    glDeleteShader(vertex_shader);
-    glDeleteShader(fragment_shader);
+    GL(glDeleteShader(vertex_shader));
+    GL(glDeleteShader(fragment_shader));
 
     GLint success;
-    glGetProgramiv(new_shader.id, GL_COMPILE_STATUS, &success);
+    GL(glGetProgramiv(new_shader.id, GL_LINK_STATUS, &success));
 
     if (!success) {
         char log[512];
-         glGetProgramInfoLog(new_shader.id, 512, NULL, log);
+        GL(glGetProgramInfoLog(new_shader.id, 512, NULL, log));
         fprintf(stderr, "%s\n", log);
     }
 
     return new_shader;
 }
 
-void shader_destroy(shader_t shader)
+void shader_destroy(const shader_t* shader)
 {
-    glDeleteProgram(shader.id);
+    GL(glDeleteProgram(shader->id));
 }
 
-void shader_bind(shader_t shader)
+void shader_bind(const shader_t* shader)
 {
-    glUseProgram(shader.id);
+    GL(glUseProgram(shader->id));
 }
 
-void shader_set_int(shader_t shader, const char* name, int value)
+void shader_set_mat4(const shader_t* shader, const char* name, mat4 value)
 {
-    glUniform1i(glGetUniformLocation(shader.id, name), value);
-}
-
-void shader_set_uint(shader_t shader, const char* name, unsigned int value)
-{
-    glUniform1ui(glGetUniformLocation(shader.id, name), value);
-}
-
-void shader_set_float(shader_t shader, const char* name, float value)
-{
-    glUniform1f(glGetUniformLocation(shader.id, name), value);
-}
-
-void shader_set_vec2(shader_t shader, const char* name, float value1, float value2)
-{
-    glUniform2f(glGetUniformLocation(shader.id, name), value1, value2);
-}
-
-void shader_set_vec3(shader_t shader, const char* name, float value1, float value2, float value3)
-{
-    glUniform3f(glGetUniformLocation(shader.id, name), value1, value2, value3);
-}
-
-void shader_set_vec4(shader_t shader, const char* name, float value1, float value2, float value3, float value4)
-{
-    glUniform4f(glGetUniformLocation(shader.id, name), value1, value2, value3, value4);
+    GL(glUniformMatrix4fv(glGetUniformLocation(shader->id, name), 1, GL_FALSE, (const GLfloat*) value));
 }
--- src/shader.h
+++ src/shader.h
@@ -1,22 +1,19 @@
 #ifndef SHADER_H
 #define SHADER_H
 
+#include <debug.h>
 #include <glad/glad.h>
 #include <glfw/glfw3.h>
 #include <stdio.h>
+#include <cglm/cglm.h>
 
 typedef struct shader {
     GLuint id;
 } shader_t;
 
 shader_t shader_create(const char* vertex_shader_source, const char* fragment_shader_source);
-void shader_destroy(shader_t shader);
-void shader_bind(shader_t shader);
-void shader_set_int(shader_t shader, const char* name, int value);
-void shader_set_uint(shader_t shader, const char* name, unsigned int value);
-void shader_set_float(shader_t shader, const char* name, float value);
-void shader_set_vec2(shader_t shader, const char* name, float value1, float value2);
-void shader_set_vec3(shader_t shader, const char* name, float value1, float value2, float value3);
-void shader_set_vec4(shader_t shader, const char* name, float value1, float value2, float value3, float value4);
+void shader_destroy(const shader_t* shader);
+void shader_bind(const shader_t* shader);
+void shader_set_mat4(const shader_t* shader, const char* name, mat4 value);
 
 #endif
