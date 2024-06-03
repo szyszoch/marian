f1ef015502a08e070bcd1490831c353aedcefc2c
szyszoch <pyrun97531@gmail.com>
2024-06-03 12:53:55 +0200

    Add settings
    The file "settings.h" contains global definitions, such as window dimensions.

---
+++ src/settings.h
@@ -0,0 +1,13 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#define WINDOW_WIDTH 1024
+#define WINDOW_HEIGHT 960
+#define WINDOW_TITLE "Marian"
+
+#define GAME_WIDTH 256
+#define GAME_HEIGHT 240
+
+#define BATCH_SIZE 256
+
+#endif
