75117a3ffd323a9a951dd4771bb119139ab92513
szyszoch <pyrun97531@gmail.com>
2024-06-04 14:42:23 +0200

    Window module

---
+++ src/window.c
@@ -0,0 +1,50 @@
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+#include "window.h"
+#include "settings.h"
+
+static GLFWwindow *window = NULL;
+
+static void resize_callback(GLFWwindow* window, int width, int height)
+{
+    (void)window; // it is used to remove warnings about not using variable
+    glViewport(0, 0, width, height);
+}
+
+int window_init()
+{
+    if (!glfwInit())
+        return -1;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE,
+                              NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        glfwTerminate();
+        return -1;
+    }
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glfwSetWindowSizeCallback(window, resize_callback);
+    return 0;
+}
+
+void window_destroy()
+{
+    glfwTerminate();
+}
+
+void window_update()
+{
+    glfwSwapBuffers(window);
+}
+
+int window_is_open()
+{
+    return !glfwWindowShouldClose(window);
+}
---
+++ src/window.h
@@ -0,0 +1,9 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+int window_init();
+void window_destroy();
+void window_update();
+int window_is_open();
+
+#endif
