f2aea5f644d727ce9a361b752e9744ad9319f05f
szyszoch <pyrun97531@gmail.com>
2024-06-06 11:37:48 +0200

    Add main loop

--- src/main.c
+++ src/main.c
@@ -1,4 +1,22 @@
-int main() 
+#include "renderer/renderer.h"
+#include "window/window.h"
+
+int main()
 {
+    window_init();
+    init_renderer();
+    set_background_color(0x94, 0x94, 0xff);
+
+    while (window_is_open())
+    {
+        window_update();
+        clear_renderer();
+        render_1_player_hud();
+        present_renderer();
+    }
+    
+    destroy_renderer();
+    window_destroy();
+
     return 0;
 }
